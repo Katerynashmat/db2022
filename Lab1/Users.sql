3bd3d876fbe41d8dd415097938ba83409d88373c
Katerynashmat <70898582+Katerynashmat@users.noreply.github.com>
2022-02-14 09:57:14 +0200

    Delete Lab1 directory

--- Lab1/Users.sql
+++
@@ -1,5 +0,0 @@
- CREATE USER user_first WITH PASSWORD '0000';
- GRANT ALL ON DATABASE "lab1" TO user_first;
- CREATE USER user_second WITH PASSWORD '2222';
- GRANT connect ON DATABASE "lab1" TO user_second;
- 
